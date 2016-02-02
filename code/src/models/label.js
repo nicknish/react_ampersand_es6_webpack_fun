032ed81bbef71d26b9e4f2e576b64a0d8749d28e
nicknish <nickjnish@gmail.com>
2016-02-02 10:12:58 -0800

    Tue Feb  2 10:12:58 PST 2016

--- code/src/models/label.js
+++ code/src/models/label.js
@@ -1,5 +1,6 @@
 import Model from 'ampersand-model'
 import githubMixin from '../helpers/github-mixin'
+import xhr from 'xhr'
 
 export default Model.extend(githubMixin, {
   // helps build the URL structure for CRUD operations
@@ -15,5 +16,27 @@ export default Model.extend(githubMixin, {
       type: 'boolean',
       default: false
     }
+  },
+
+  update (attributes) {
+    
+    const oldAttributes = this.getAttributes({props: true, session: false})
+
+    xhr({
+      // based off of parent collection url()
+      url: this.url(),
+      json: attributes,
+      method: 'PATCH',
+      headers: {
+        'Authorization': 'token ' + app.me.token
+      }
+    }, (err, req, body) => {
+      // if error out, revert back
+      if (err) {
+        this.set(oldAttributes)
+        console.log('something went wrong, check your wifi')
+      }
+    })
+    this.set(attributes)
   }
 })
