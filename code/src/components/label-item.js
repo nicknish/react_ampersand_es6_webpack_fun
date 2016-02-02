da1865a58544ed2cfae6e807a13f0f3a4b9e7a3c
nicknish <nickjnish@gmail.com>
2016-02-02 00:35:24 -0800

    Tue Feb  2 00:35:24 PST 2016

--- code/src/components/label-item.js
+++ code/src/components/label-item.js
@@ -19,6 +19,20 @@ export default React.createClass({
     this.props.label.destroy()
   },
 
+  getInitialState () {
+    const {name, color} = this.props.label
+    
+    return {name, color}
+  },
+
+  onNameChange (event) {
+    // React will rerender state
+    this.setState({
+      // event.target.value grabs input value
+      name: event.target.value
+    })
+  },
+
   render () {
     const {label} = this.props
     const cssColor = '#' + label.color
@@ -29,8 +43,8 @@ export default React.createClass({
       content = (
         <form className='label'>
           <span className='label-color avatar avatar-small avatar-rounded'>&nbsp;</span>
-          <input name='name'/>
-          <input name='color'/>
+          <input name='name' onChange={this.onNameChange} value={this.state.name}/>
+          <input name='color' value={this.state.color}/>
           <button type='submit' className='button button-small'>Save</button>
           <button onClick={this.onCancelClick} type='button' className='button button-small button-unstyled'>cancel</button>
         </form>
