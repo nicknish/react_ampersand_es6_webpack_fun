81b0608852bb0545a6ed710b4496e27abc137271
nicknish <nickjnish@gmail.com>
2016-02-02 00:12:50 -0800

    Tue Feb  2 00:12:50 PST 2016

--- code/src/models/label.js
+++ code/src/models/label.js
@@ -2,6 +2,7 @@ import Model from 'ampersand-model'
 import githubMixin from '../helpers/github-mixin'
 
 export default Model.extend(githubMixin, {
+  // helps build the URL structure for CRUD operations
   idAttribute: 'name',
 
   props: {
