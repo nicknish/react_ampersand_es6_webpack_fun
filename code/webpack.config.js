9c61dba1a4e281513780b8b183c2c99fbb1aec7d
nicknish <nickjnish@gmail.com>
2016-02-02 13:06:30 -0800

    Tue Feb  2 13:06:30 PST 2016

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -1,12 +1,19 @@
+// use babel to rewrite Node's require, so we can use PublicPage
+require('babel/register')
 var getConfig = require('hjs-webpack')
+var React = require('react')
+var PublicPage = require('./src/pages/public')
 
 module.exports = getConfig({
 	in: 'src/app.js',
   out: 'public',
   clearBeforeBuild: true,
   html: function(context) {
+    // no JSX allowed here, let's write what JSX would render to
+    const publicPage = React.renderToString(React.createElement(PublicPage))
+
     return {
-      'index.html': context.defaultTemplate(),
+      'index.html': context.defaultTemplate({html: publicPage}),
       '200.html': context.defaultTemplate()
     }
   }
