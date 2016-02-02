cca137c30802c6f43de32aa57aadb22fa875ad99
nicknish <nickjnish@gmail.com>
2016-02-01 20:48:15 -0800

    Added editing state

--- code/src/components/label-item.js
+++ code/src/components/label-item.js
@@ -4,7 +4,33 @@ export default React.createClass({
   
   render () {
     const {label} = this.props  
-  
-    return (<li>{label.name}</li>)
+    let cssColor = '#' + label.color
+    let content
+
+    // editing
+    if (label.editing) {
+      content = (
+        <form className='label'>
+          <span className='label-color avatar avatar-small avatar-rounded'>&nbsp;</span>
+          <input name='name'/>
+          <input name='color'/>
+          <button type='submit' className='button button-small'>Save</button>
+          <button type='button' className='button button-small button-unstyled'>cancel</button>
+        </form>
+      )
+    } else {
+      content = (
+        <div className='label'>
+          <span className='label-color' style={{backgroundColor: cssColor}}>&nbsp;</span>
+          <span>{label.name}</span>
+          <span className='octicon octicon-pencil'></span>
+          <span className='octicon octicon-x'></span>
+        </div>
+      )
+    }
+
+    return (
+      <div>{content}</div>
+    )
   }
 })
--- code/src/models/label.js
+++ code/src/models/label.js
@@ -5,5 +5,12 @@ export default Model.extend({
   props: {
     name: 'string',
     color: 'string'
+  },
+
+  session: {
+    editing: {
+      type: 'boolean',
+      default: false
+    }
   }
 })
