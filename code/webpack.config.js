98ea8ee63523f41916b6057abb5b4df17e22f5a5
nicknish <nickjnish@gmail.com>
2016-02-02 13:29:04 -0800

    Tue Feb  2 13:29:04 PST 2016

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -3,6 +3,7 @@ require('babel/register')
 var getConfig = require('hjs-webpack')
 var React = require('react')
 var PublicPage = require('./src/pages/public')
+var Layout = require('./src/layout')
 
 module.exports = getConfig({
 	in: 'src/app.js',
@@ -12,9 +13,12 @@ module.exports = getConfig({
     // no JSX allowed here, let's write what JSX would render to
     const publicPage = React.renderToString(React.createElement(PublicPage))
 
+    // pass in data, doesn't need to have data as long as it doesn't blow up
+    const layoutPage = React.renderToString(React.createElement(Layout, {me: {}}))
+
     return {
       'index.html': context.defaultTemplate({html: publicPage}),
-      '200.html': context.defaultTemplate()
+      '200.html': context.defaultTemplate({html: layoutPage})
     }
   }
 })
